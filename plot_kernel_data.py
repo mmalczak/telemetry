4b764937ec88f9c954d3c26efe37890f75430070
Miłosz Malczak <milosz.malczak@nask.pl>
2021-01-18 13:58:32 +0100

    Fixed style

--- plot_kernel_data.py
+++ plot_kernel_data.py
@@ -1,41 +1,41 @@
-import os
-import matplotlib.pyplot as plt
 import statistics
-import scipy.signal as dsp
+
+import matplotlib.pyplot as plt
 import numpy as np
+import scipy.signal as dsp
+
 
 def get_array(x):
-    x = x.replace('[', '')
-    x = x.replace(']', '')
-    x = x.split(',')
+    x = x.replace("[", "")
+    x = x.replace("]", "")
+    x = x.split(",")
     x = [float(el) for el in x]
+
     return x
 
-x_beg = 00;
-x_end = 100;
 
-time = [];
+x_beg = 00
+x_end = 100
+
+time = []
 load_est = []
 load = []
 uc = []
-freq = [];
-freq_next = [];
-R = [];
-S = [];
-T = [];
-D = [];
-P = [];
-phi_P_phi = [];
-theta = [];
-stop = 1;
-with open('data.txt') as f:
+freq = []
+freq_next = []
+R = []
+S = []
+T = []
+D = []
+P = []
+phi_P_phi = []
+theta = []
+with open("data.txt") as f:
     lines = f.readlines()
+
     for line in lines:
-        line_split = line.split(';')
-        #if stop:
-        #    print(line_split)
-        #    stop = 0
-        time.append(float(line_split[0])+float(line_split[1])/1000000)
+        line_split = line.split(";")
+        time.append(float(line_split[0]) + float(line_split[1]) / 1000000)
         load.append(float(line_split[3]))
         load_est.append(float(line_split[4]))
         uc.append(float(line_split[5]))
@@ -49,132 +49,103 @@ with open('data.txt') as f:
         P.append(get_array(line_split[13]))
         phi_P_phi.append(get_array(line_split[14]))
 
-print(statistics.mean(load));
-print(statistics.mean(load_est));
+print(statistics.mean(load))
+print(statistics.mean(load_est))
 
 a = np.array([1])
-l = 1;
-b = np.ones(l, dtype=float)/l
+L = 1
+b = np.ones(L, dtype=float) / L
 a.astype(float)
-load_est = dsp.lfilter(b, a, load_est);
-load= dsp.lfilter(b, a, load);
+load_est = dsp.lfilter(b, a, load_est)
+load = dsp.lfilter(b, a, load)
 
-#N=500
-#plt.figure(0)
-#axes = plt.gca()
-#axes.set_ylim([0,200])
-#plt.plot(time, load, label='load');
-#plt.plot(time, load_est, label='load_est');
-#plt.plot(time, uc, label='uc');
-#plt.grid()
-#plt.legend(loc='upper left')
+N = len(time)
+x_beg = int(N * x_beg / 100)
+x_end = int(N * x_end / 100)
 
-
-N = len(time);
-x_beg = int(N*x_beg/100);
-x_end = int(N*x_end/100);
-
-N_subplot = 6;
+N_SUBPLOT = 6
 
 plt.figure(1)
-sp = plt.subplot(N_subplot, 1, 1)
+sp = plt.subplot(N_SUBPLOT, 1, 1)
 sp.set_title("Output")
 axes = plt.gca()
-axes.set_ylim([0,200])
-plt.plot(time[x_beg:x_end], load[x_beg:x_end], label='y');
-#plt.plot(time[x_beg:x_end], load_est[x_beg:x_end], label='y_est');
-plt.plot(time[x_beg:x_end], uc[x_beg:x_end], label='uc');
+axes.set_ylim([0, 200])
+plt.plot(time[x_beg:x_end], load[x_beg:x_end], label="y")
+# plt.plot(time[x_beg:x_end], load_est[x_beg:x_end], label='y_est')
+plt.plot(time[x_beg:x_end], uc[x_beg:x_end], label="uc")
 plt.grid()
-plt.legend(loc='upper left')
+plt.legend(loc="upper left")
 
-#sp = plt.subplot(N_subplot, 1, 2)
-#sp.set_title("Input")
-#axes = plt.gca()
-##axes.set_ylim([0,5000000])
-#plt.plot(time[x_beg:x_end], freq_next[x_beg:x_end], label='v');
-#plt.plot(time[x_beg:x_end], freq[x_beg:x_end], label='u');
-#plt.grid()
-#plt.legend(loc='upper left')
-
-sp = plt.subplot(N_subplot, 1, 2)
+sp = plt.subplot(N_SUBPLOT, 1, 2)
 sp.set_title("Input scaled")
 axes = plt.gca()
-axes.set_ylim([0,5000000])
-plt.plot(time[x_beg:x_end], freq_next[x_beg:x_end], label='v');
-plt.plot(time[x_beg:x_end], freq[x_beg:x_end], label='u');
+axes.set_ylim([0, 5000000])
+plt.plot(time[x_beg:x_end], freq_next[x_beg:x_end], label="v")
+plt.plot(time[x_beg:x_end], freq[x_beg:x_end], label="u")
 plt.grid()
-plt.legend(loc='upper left')
+plt.legend(loc="upper left")
 
-
-sp = plt.subplot(N_subplot, 1, 3)
+sp = plt.subplot(N_SUBPLOT, 1, 3)
 sp.set_title("Estimated parameters")
-plt.plot(time[x_beg:x_end], theta[x_beg:x_end]);
+plt.plot(time[x_beg:x_end], theta[x_beg:x_end])
 plt.grid()
 leg = []
+
 for i in range(len(theta[0])):
-    leg.append("theta["+str(i)+"]")
-plt.legend(leg, loc='upper left')
+    leg.append("theta[" + str(i) + "]")
+plt.legend(leg, loc="upper left")
 
-#sp = plt.subplot(N_subplot, 1, 5)
-#sp.set_title("Covariance matrix")
-#plt.plot(time[x_beg:x_end], P[x_beg:x_end]);
-##leg = []
-##for i in range(len(theta)):
-##    leg.append("P["+str(i)+"]")
-##plt.legend(leg, loc='upper left')
-
-sp = plt.subplot(N_subplot, 1, 4)
+sp = plt.subplot(N_SUBPLOT, 1, 4)
 sp.set_title("Controller")
 axes = plt.gca()
-axes.set_ylim([-10,10])
-plt.plot(time[x_beg:x_end], R[x_beg:x_end]);
-plt.plot(time[x_beg:x_end], S[x_beg:x_end]);
-plt.plot(time[x_beg:x_end], T[x_beg:x_end]);
+axes.set_ylim([-10, 10])
+plt.plot(time[x_beg:x_end], R[x_beg:x_end])
+plt.plot(time[x_beg:x_end], S[x_beg:x_end])
+plt.plot(time[x_beg:x_end], T[x_beg:x_end])
 leg = []
+
 for i in range(len(R[0])):
-    leg.append("R["+str(i)+"]")
-for i in range(len(S[0])):
-    leg.append("S["+str(i)+"]")
-for i in range(len(T[0])):
-    leg.append("T["+str(i)+"]")
-plt.legend(leg, loc='upper left')
+    leg.append("R[" + str(i) + "]")
 
-sp = plt.subplot(N_subplot, 1, 5)
+for i in range(len(S[0])):
+    leg.append("S[" + str(i) + "]")
+
+for i in range(len(T[0])):
+    leg.append("T[" + str(i) + "]")
+plt.legend(leg, loc="upper left")
+
+sp = plt.subplot(N_SUBPLOT, 1, 5)
 sp.set_title("Controller")
 axes = plt.gca()
-axes.set_ylim([-10,10])
-plt.plot(time[x_beg:x_end], D[x_beg:x_end]);
+axes.set_ylim([-10, 10])
+plt.plot(time[x_beg:x_end], D[x_beg:x_end])
 leg = []
-for i in range(len(D[0])):
-    leg.append("D["+str(i)+"]")
-plt.legend(leg, loc='upper left')
 
-sp = plt.subplot(N_subplot, 1, 6)
+for i in range(len(D[0])):
+    leg.append("D[" + str(i) + "]")
+plt.legend(leg, loc="upper left")
+
+sp = plt.subplot(N_SUBPLOT, 1, 6)
 sp.set_title("phi_P_phi")
 axes = plt.gca()
-#axes.set_ylim([-20000,-50000])
-plt.plot(time[x_beg:x_end], phi_P_phi[x_beg:x_end]);
-plt.hlines(1000, time[x_beg], time[x_end-1]);
-#leg = []
-#for i in range(len(D[0])):
-#    leg.append("D["+str(i)+"]")
-#plt.legend(leg, loc='upper left')
+plt.plot(time[x_beg:x_end], phi_P_phi[x_beg:x_end])
+plt.hlines(1000, time[x_beg], time[x_end - 1])
 
 plt.grid()
 plt.show()
 
-#plt.figure(2)
-#sp = plt.subplot()
-#ax = plt.gca()
-#ax.set_xlim((-5, 5))
-#ax.set_ylim((-2.5, 2.5))
-#sp.set_title("Model poles and zeros")
-#zeros_re = []
-#zeros_im = []
-#poles_re = []
-#poles_im = []
+# plt.figure(2)
+# sp = plt.subplot()
+# ax = plt.gca()
+# ax.set_xlim((-5, 5))
+# ax.set_ylim((-2.5, 2.5))
+# sp.set_title("Model poles and zeros")
+# zeros_re = []
+# zeros_im = []
+# poles_re = []
+# poles_im = []
 ##theta = theta[0:1]
-#for idx, theta_el in enumerate(theta):
+# for idx, theta_el in enumerate(theta):
 #    A = [1] + theta_el[0:2]
 #    B = theta_el[2:]
 #    z = np.roots(B)
@@ -183,9 +154,9 @@ plt.show()
 #    zeros_im.append(z.imag)
 #    poles_re.append(p.real)
 #    poles_im.append(p.imag)
-#circle1 = plt.Circle((0, 0), 1, fill=False)
-#ax.add_artist(circle1)
-#plt.scatter(poles_re, poles_im, marker = 'x', color='r')
-#plt.scatter(zeros_re, zeros_im, marker = 'o', color='none', edgecolor='b')
-#plt.grid()
-#plt.show()
+# circle1 = plt.Circle((0, 0), 1, fill=False)
+# ax.add_artist(circle1)
+# plt.scatter(poles_re, poles_im, marker = 'x', color='r')
+# plt.scatter(zeros_re, zeros_im, marker = 'o', color='none', edgecolor='b')
+# plt.grid()
+# plt.show()
--- tlm.c
+++ tlm.c
@@ -23,205 +23,208 @@
 // accepted data formats
 enum fmt_t {CSV, HR};
 
-#define max(a,b) \
-  ({ __typeof__ (a) _a = (a); \
-      __typeof__ (b) _b = (b); \
+#define max(a,b)		\
+  ({ __typeof__ (a) _a = (a);	\
+      __typeof__ (b) _b = (b);	\
     _a > _b ? _a : _b; })
 
-#define min(a,b) \
-  ({ __typeof__ (a) _a = (a); \
-      __typeof__ (b) _b = (b); \
+#define min(a,b)		\
+  ({ __typeof__ (a) _a = (a);	\
+      __typeof__ (b) _b = (b);	\
     _a < _b ? _a : _b; })
 
 int help(char *appName)
 {
-  printf(  "Kernel telemetry reader."                     \
-    "  Copyright (C)  2017 Michał Getka\n\n"              \
-    "\tProvides tlm LKM buffer to stdout with "           \
-    "specified formatting.\n\n");                         \
-  printf("\tThis tlm application is appropriate to read"  \
-    "\n\tdata from tlmsrv of type %s.\n\n", DEVICE_NAME); \
-  printf(  "usage:\t%s [-option <arguments>]\n"           \
-    "\t-h\t\tPrint out this message\n"                    \
-    "\t-p\t\tPrint model parameters to a file\n"          \
-    "\t-d DEVICE\tRead data from tlm server DEVICE.\n"    \
-    "\t\t\tif not set, DEVICE defaults to \\dev\\%s\n"    \
-    "\t-r\t\treset tlm srerver buffer.\n"                 \
-    "\t-l\t\tinclude labels of timeseries signals\n"      \
-    "\t-f FORMAT\tSets output format type\n"              \
-    "\tArguments:\n"                                      \
-    "\t hr - human readable (default)\n"                  \
-    "\t csv - comma separated values\n"                   \
-    "\nExit status:\n"                                    \
-      "-lost_frames\tif OK,\n"                            \
-      ">0\t\tif problems,\n"                              \
-    "\n\tReport bugs to michal.getka[at]gmail.com\n", appName, DEVICE_NAME);
-
-  exit(0);
-
+	printf(  "Kernel telemetry reader."				\
+		"  Copyright (C)  2017 Michał Getka\n\n"		\
+		"\tProvides tlm LKM buffer to stdout with "		\
+		"specified formatting.\n\n");				\
+	printf("\tThis tlm application is appropriate to read"		\
+		"\n\tdata from tlmsrv of type %s.\n\n", DEVICE_NAME);	\
+	printf(  "usage:\t%s [-option <arguments>]\n"			\
+		"\t-h\t\tPrint out this message\n"			\
+		"\t-p\t\tPrint model parameters to a file\n"		\
+		"\t-d DEVICE\tRead data from tlm server DEVICE.\n"	\
+		"\t\t\tif not set, DEVICE defaults to \\dev\\%s\n"	\
+		"\t-r\t\treset tlm srerver buffer.\n"			\
+		"\t-l\t\tinclude labels of timeseries signals\n"	\
+		"\t-f FORMAT\tSets output format type\n"		\
+		"\tArguments:\n"					\
+		"\t hr - human readable (default)\n"			\
+		"\t csv - comma separated values\n"			\
+		"\nExit status:\n"					\
+			"-lost_frames\tif OK,\n"			\
+			">0\t\tif problems,\n"				\
+		"\n\tReport bugs to michal.getka[at]gmail.com\n", appName,
+								DEVICE_NAME);
+	exit(0);
 }
 
 int main(int argc, char *argv[])
 {
+	unsigned int i, numel;
+	int fd, ret, rc = 0;
+	char *program = strdup(argv[0]), *appName=basename(program);
+	char *devname = "/dev/" DEVICE_NAME;
+	int labels = 0;
+	enum fmt_t format = HR;
+	int show_output = 1;
+	char option;
+	int model_params = 0;
 
-  unsigned int i, numel;
-  int fd, ret, rc   = 0;
-  char *program     = strdup(argv[0]), *appName=basename(program);
-  char *devname     = "/dev/" DEVICE_NAME;
-  int labels        = 0;
-  enum fmt_t format = HR;
-  int show_output   = 1;
-  char option;
-  int model_params = 0;
+	int next_arg = 1;
 
-  int next_arg = 1;
+	// parameters handling
+	while (next_arg < argc && argv[next_arg][0] == '-') {
 
-  // parameters handling
-  while (next_arg < argc && argv[next_arg][0] == '-') {
+	option = argv[next_arg][1];
 
-    option = argv[next_arg][1];
+	switch (option) {
+	case 'h':
+		help(appName);
+		// this is actually unnecessary.
+		// help function terminates application.
+		next_arg += 1;
+		break;
+	case 'p':
+		model_params = 1;
+		show_output = 0;
+		next_arg++;
+		break;
+	case 'd':
+		if (next_arg + 1 < argc) {
+			devname = argv[ next_arg + 1 ];
+		} else {
+			printf("Invalid device name...\n");
+			rc = -EINVAL;
+			goto invalid_argument;
+		}
+		next_arg += 2;
+		break;
+	case 'r':
+		show_output = 0;
+		next_arg += 1;
+		break;
+	case 'f':
+		if (next_arg + 1 < argc) {
+			if (strcmp(argv[ next_arg + 1 ], "csv") == 0)
+				format = CSV;
+			else if (strcmp(argv[ next_arg + 1 ], "hr") == 0)
+				format = HR;
+				else {
+					printf("Invalid format name...\n");
+				rc = -EINVAL;
+				goto invalid_argument;
+				}
+		} else {
+			printf("Invalid format name...\n");
+			rc = -EINVAL;
+			goto invalid_argument;
+		}
+		next_arg += 2;
+		break;
+	case 'l':
+		labels = 1;
+		next_arg += 1;
+		break;
+	}
+	};
 
-    switch (option) {
-      case 'h':
-        help(appName);
-        // this is actually unnecessary. help function terminates application.
-        next_arg += 1;
-        break;
-      case 'p':
-        model_params = 1;
-        show_output = 0;
-        next_arg++;
-        break;
-      case 'd':
-        if (next_arg + 1 < argc) {
-          devname = argv[ next_arg + 1 ];
-        } else {
-          printf("Invalid device name...\n");
-          rc = -EINVAL;
-          goto invalid_argument;
-        }
-        next_arg += 2;
-        break;
-      case 'r':
-        show_output = 0;
-        next_arg += 1;
-        break;
-      case 'f':
-        if (next_arg + 1 < argc) {
-          if (strcmp(argv[ next_arg + 1 ], "csv") == 0)
-            format = CSV;
-          else if (strcmp(argv[ next_arg + 1 ], "hr") == 0)
-            format = HR;
-          else {
-            printf("Invalid format name...\n");
-            rc = -EINVAL;
-            goto invalid_argument;
-          }
-        } else {
-          printf("Invalid format name...\n");
-          rc = -EINVAL;
-          goto invalid_argument;
-        }
-        next_arg += 2;
-        break;
-      case 'l':
-        labels = 1;
-        next_arg += 1;
-        break;
-    }
+	// if device name is set ...
+	if (devname) {
+		FILE * fp;
+		fp = fopen("params.txt", "w");
+		fd = open(devname, O_RDONLY);
 
-  };
+		if (fd < 0) {
+			perror("Failed to open the device...");
+			return errno;
+		}
+		ret = read(fd, &tlm, sizeof(struct tlm_private));
+		if (ret < 0) {
+			perror("Failed to communicate with the device...");
+			close(fd);
+			return errno;
+		}
+		if(model_params)
+		{
+			numel = min(tlm.data_count, TLM_BUFFER_SIZE);
 
-  // if device name is set ...
-  if (devname) {
-    FILE * fp;
-    fp = fopen("params.txt", "w");
-    fd = open(devname, O_RDONLY);
+			for (i = 0; i<numel; i++){
+				fprintf(fp, "%ld, %ld\n", tlm.data[i].theta[0],
+							tlm.data[i].theta[1]);
+			}
 
-    if (fd < 0) {
-      perror("Failed to open the device...");
-      return errno;
-    }
+		}
+		if (show_output) {
+			// For the case when some trash data are loaded
+			// instead of valid tlm,
+			// eg ./tlm -d /dev/sda1
+			numel = min(tlm.data_count, TLM_BUFFER_SIZE);
 
-    ret = read(fd, &tlm, sizeof(struct tlm_private));
+			switch (format) {
+			case HR:
 
-    if (ret < 0) {
-      perror("Failed to communicate with the device...");
-      close(fd);
-      return errno;
-    }
+				printf("Retrieved samples:\t%d\n",
+								tlm.data_count);
+				printf("Missed samples:\t\t%d\n\n",
+								tlm.data_lost);
 
-    if(model_params)
-    {
-        numel = min(tlm.data_count, TLM_BUFFER_SIZE);
+				printf("%s\t\t","ts_sec");
+				printf("%s\t","ts_usec");
+				for (i = 0; labelsDef[i]; i++)
+				{
+					printf("%s\t", labelsDef[i]);
+					int j;
+					if(!strcmp("theta", labelsDef[i]))
+						for(j=0; j<deg-1; j++)
+							printf("\t");
+					if(!strcmp("R", labelsDef[i]))
+						for(j=0; j<d_R; j++)
+							printf("\t");
+					if(!strcmp("S", labelsDef[i]))
+						for(j=0; j<d_S; j++)
+							printf("\t");
+					if(!strcmp("T", labelsDef[i]))
+						for(j=0; j<d_T; j++)
+							printf("\t");
+					if(!strcmp("D", labelsDef[i]))
+						for(j=0; j<d_D; j++)
+							printf("\t");
+				}
+				printf("\n");
 
-        for (i = 0; i<numel; i++){
-          fprintf(fp, "%ld, %ld\n", tlm.data[i].theta[0], tlm.data[i].theta[1]);
-        }
-
-    }
-    if (show_output) {
-
-      // For the case when some trash data are loaded instead of valid tlm,
-      // eg ./tlm -d /dev/sda1
-      numel = min(tlm.data_count, TLM_BUFFER_SIZE);
-
-      switch (format) {
-        case HR:
-
-          printf("Retrieved samples:\t%d\n", tlm.data_count);
-          printf("Missed samples:\t\t%d\n\n", tlm.data_lost);
-
-          printf("%s\t\t","ts_sec");
-          printf("%s\t","ts_usec");
-          for (i = 0; labelsDef[i]; i++)
-          {
-            printf("%s\t", labelsDef[i]);
-            int j;
-            if(!strcmp("theta", labelsDef[i])) for(j=0; j<deg-1; j++)printf("\t");
-            if(!strcmp("R", labelsDef[i])) for(j=0; j<d_R; j++)printf("\t");
-            if(!strcmp("S", labelsDef[i])) for(j=0; j<d_S; j++)printf("\t");
-            if(!strcmp("T", labelsDef[i])) for(j=0; j<d_T; j++)printf("\t");
-            if(!strcmp("D", labelsDef[i])) for(j=0; j<d_D; j++)printf("\t");
-          }
-          printf("\n");
-
-          for (i = 0; i<numel; i++) {
-            printf("%lld\t%d\t", tlm.data[i].ts_sec, tlm.data[i].ts_usec);
-            print_sample(tlm.data + i, "\t", 1);
-            printf("\n");
-          }
-
-          break;
-        case CSV:
-
-          if (labels) {
-            printf("%s,","ts_sec");
-            printf("%s","ts_usec");
-            for (i = 0; labelsDef[i]; i++)
-              printf(",%s", labelsDef[i]);
-            printf("\n");
-          }
-
-          for (i = 0; i<numel; i++){
-            printf("%lld;%d;", tlm.data[i].ts_sec, tlm.data[i].ts_usec);
-            print_sample(tlm.data + i, ";", 0);
-            printf("\n");
-          }
-
-          break;
-      }
-
-
-      rc = -tlm.data_lost;
-
-    }
-
-    close(fd);
-    fclose(fp);
-  }
+				for (i = 0; i<numel; i++) {
+					printf("%lld\t%d\t",
+					       tlm.data[i].ts_sec,
+							tlm.data[i].ts_usec);
+					print_sample(tlm.data + i, "\t", 1);
+					printf("\n");
+				}
+				break;
+			case CSV:
+				if (labels) {
+					printf("%s,","ts_sec");
+					printf("%s","ts_usec");
+					for (i = 0; labelsDef[i]; i++)
+						printf(",%s", labelsDef[i]);
+					printf("\n");
+				}
+				for (i = 0; i<numel; i++){
+					printf("%lld;%d;", tlm.data[i].ts_sec,
+							tlm.data[i].ts_usec);
+					print_sample(tlm.data + i, ";", 0);
+					printf("\n");
+				}
+				break;
+			}
+			rc = -tlm.data_lost;
+		}
+		close(fd);
+		fclose(fp);
+	}
 
 invalid_argument:
-  free(program);
-  return rc;
+	free(program);
+	return rc;
 }
